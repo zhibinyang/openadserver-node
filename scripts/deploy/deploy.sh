fa2f4fe70ab2675cb89a154d550eb89c3a866e81
zhibinyang <yang_zhibin824@qq.com>
2026-03-14 07:52:34 +0800

    update deploy model script

--- scripts/deploy/deploy.sh
+++ scripts/deploy/deploy.sh
@@ -1,4 +1,4 @@
-export GOOGLE_APPLICATION_CREDENTIALS=/home/yzb7530326309/.config/credential.json
-gcloud storage cp gs://openadserver-training-models/models/pctr/feature_config_deepfm_latest.json /home/yzb7530326309/openadserver-node/models/feature_config_latest.json
-gcloud storage cp gs://openadserver-training-models/models/pctr/deepfm_model_latest.onnx /home/yzb7530326309/openadserver-node/models/pctr_deepfm_model_latest.onnx
-gcloud storage cp gs://openadserver-training-models/models/pcvr/deepfm_model_latest.onnx /home/yzb7530326309/openadserver-node/models/pcvr_deepfm_model_latest.onnx
+export GOOGLE_APPLICATION_CREDENTIALS=~/.config/credential.json
+gcloud storage cp gs://openadserver-training-models/models/pctr/feature_config_deepfm_latest.json ../../models/feature_config_latest.json
+gcloud storage cp gs://openadserver-training-models/models/pctr/deepfm_model_latest.onnx ../../models/pctr_deepfm_model_latest.onnx
+gcloud storage cp gs://openadserver-training-models/models/pcvr/deepfm_model_latest.onnx ../../models/pcvr_deepfm_model_latest.onnx
